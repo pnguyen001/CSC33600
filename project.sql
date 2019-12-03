c37f0c23ece9bbb7395af41eae798aef321bb25e
PhucNguyen <pnguyen001@citymail.cuny.edu>
2019-12-02 19:07:53 -0500

    add data types

--- project.sql
+++ project.sql
@@ -4,44 +4,43 @@ USE sudo_project;
 
 DROP TABLE IF EXISTS department;
 CREATE TABLE department(
-	dept_no,
-	dept_name
+	dept_no INT,
+	dept_name CHAR(20)
 );
 DROP TABLE IF EXISTS dept_emp;
 CREATE TABLE dept_emp(
-	emp_id,
-	dept_no
-	from_date,
-	to_date
+	emp_id INT,
+	dept_no INT,
+	from_date DATE,
+	to_date DATE
 );
 DROP TABLE IF EXISTS empt_manager;
 CREATE TABLE empt_manger(
-	emp_id,
-	dept_no
-	from_date,
-	to_date
+	emp_id INT,
+	from_date DATE,
+	to_date DATE
 );
 DROP TABLE IF EXISTS employees;
 CREATE TABLE employees(
-	emp_id,
-	name,
-	dob,
-	dept_no,
-	gender,
-	hire_date
+	emp_id INT,
+	name CHAR(30),
+	dob DATE,
+	dept_no INT,
+	gender CHAR(8),
+	hire_date DATE
 );
 DROP TABLE IF EXISTS salaries;
 CREATE TABLE salaries(
-	emp_id,
-	salary,
-	from_date,
-	to_date
+	emp_id INT,
+	salary money INT,
+	from_date DATE,
+	to_date DATE
 );
 DROP TABLE IF EXISTS title;
 CREATE TABLE title(
-	emp_id,
-	title,
-	from_date,
-	to_date
+	emp_id INT,
+	title CHAR(10),
+	from_date DATE,
+	to_date DATE
 );
 
